2932dc527c1ac80867573c05f5e3c9d3d3578c07
22jh0369 <22jh0369@barton.ac.uk>
2023-07-05 10:17:46 +0100

    Work in progress UI, Matrices

---
+++ NEAConsole/Matrices/Matrix.cs
@@ -0,0 +1,267 @@
+﻿namespace NEAConsole.Matrices;
+
+public class DimensionLessThanOneException : Exception { }
+public class MatrixNotSquareException : Exception { }
+public class MatrixSingularException : Exception { }
+public class Matrix
+{
+    private readonly double[,] values;
+    public readonly int Rows;
+    public readonly int Columns;
+    public readonly bool Square;
+    public double Determinant
+    {
+        get
+        {
+            if (!Square) throw new MatrixNotSquareException();
+            if (Rows == 1) return values[0, 0];
+            if (Rows == 2) return values[0, 0] * values[1, 1] - values[0, 1] * values[1, 0];
+
+            double determinant = 0;
+            double[] topRow = GetRow(0).ToArray();
+            for (int i = 0; i < Columns; i++)
+            {
+                double scalar = topRow[i];
+                Matrix minor = Minor(0, i);
+
+                if (i % 2 == 0)
+                    determinant += scalar * minor.Determinant;
+                else
+                    determinant -= scalar * minor.Determinant;
+            }
+
+            return determinant;
+        }
+    }
+
+    public Matrix Transpose
+    {
+        get
+        {
+            if (!Square) throw new MatrixNotSquareException();
+            Matrix m = new(Rows);
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    m[i, j] = values[j, i];
+                }
+            }
+
+            return m;
+        }
+    }
+
+    public Matrix Adjugate
+    {
+        get
+        {
+            if (!Square) throw new MatrixNotSquareException();
+            Matrix t = Transpose;
+            Matrix m = new(Rows);
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    double minor = t.Minor(i, j).Determinant;
+                    int cofactor = i % 2 == 0 ? j % 2 == 0 ? 1 : -1 : j % 2 == 0 ? -1 : 1;
+                    m[i, j] = cofactor * minor;
+                }
+            }
+
+            return m;
+        }
+    }
+
+    public Matrix Inverse
+    {
+        get
+        {
+            double det = Determinant;
+            if (det == 0) throw new MatrixSingularException();
+            return 1 / det * Adjugate;
+        }
+    }
+
+    public static Matrix Identity(int dimension)
+    {
+        Matrix m = Zero(dimension);
+        for (int i = 0; i < dimension; i++) { m[i, i] = 1; }
+
+        return m;
+    }
+    public static Matrix Zero(int dimension) => new(dimension, dimension);
+
+    public Matrix Minor(int row, int col)
+    {
+        if (!Square) throw new MatrixNotSquareException();
+        List<double> contents = new((row - 1) * (row - 1));
+        for (int i = 0; i < Rows; i++)
+        {
+            if (i == row) continue;
+            for (int j = 0; j < Columns; j++)
+            {
+                if (j == col) continue;
+                contents.Add(values[i, j]);
+            }
+        }
+
+        return new Matrix(Rows - 1, Columns - 1, contents);
+    }
+
+    public double this[int i, int j]
+    {
+        get => values[i, j];
+        set => values[i, j] = value;
+    }
+
+    public Matrix(IEnumerable<IEnumerable<double>> vals) : this(vals.Count(), vals.First().Count(), vals) { }
+    public Matrix(int rows, int cols, IEnumerable<IEnumerable<double>> vals)
+    {
+        Rows = rows;
+        Columns = cols;
+        Square = Rows == Columns;
+        values = new double[Rows, Columns];
+
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                values[i, j] = vals.ElementAt(i).ElementAt(j);
+            }
+        }
+    }
+
+    public Matrix(int rows, int cols, IEnumerable<double> vals) : this(rows, cols)
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                values[i, j] = vals.ElementAt(i * Columns + j);
+            }
+        }
+    }
+
+    public Matrix(double[,] vals)
+    {
+        Rows = vals.GetLength(0);
+        Columns = vals.GetLength(1);
+        Square = Rows == Columns;
+        values = vals;
+    }
+
+    public Matrix(int rows, int cols)
+    {
+        if (rows < 1 || cols < 1) throw new DimensionLessThanOneException();
+        Rows = rows;
+        Columns = cols;
+        Square = rows == cols;
+        values = new double[Rows, Columns];
+    }
+
+    public Matrix(int dimension)
+    {
+        if (dimension < 1) throw new DimensionLessThanOneException();
+        Rows = dimension;
+        Columns = dimension;
+        Square = true;
+        values = new double[Rows, Columns];
+    }
+
+    public static Matrix operator -(Matrix m)
+    {
+        Matrix output = new(m.Rows, m.Columns);
+
+        for (int i = 0; i < m.Rows; i++)
+        {
+            for (int j = 0; j < m.Columns; j++)
+            {
+                output[i, j] = -m[i, j];
+            }
+        }
+
+        return m;
+    }
+
+    public static Matrix operator +(Matrix a, Matrix b)
+    {
+        Matrix m = new(a.Rows, a.Columns);
+
+        for (int i = 0; i < a.Rows; i++)
+        {
+            for (int j = 0; j < a.Columns; j++)
+            {
+                m[i, j] = a[i, j] + b[i, j];
+            }
+        }
+
+        return m;
+    }
+
+    public static Matrix operator -(Matrix a, Matrix b) => a + -b;
+
+    public IEnumerable<double> GetRow(int rowIndex)
+    {
+        for (int i = 0; i < Columns; i++)
+        {
+            yield return values[rowIndex, i];
+        }
+    }
+    public IEnumerable<double> GetCol(int colIndex)
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            yield return values[i, colIndex];
+        }
+    }
+
+    public static Matrix operator *(double scalar, Matrix matrix)
+    {
+        Matrix m = new(matrix.Rows, matrix.Columns);
+
+        for (int i = 0; i < matrix.Rows; i++)
+        {
+            for (int j = 0; j < matrix.Columns; j++)
+            {
+                m[i, j] = scalar * matrix[i, j];
+            }
+        }
+
+        return m;
+    }
+
+    public static Matrix operator *(Matrix a, Matrix b)
+    {
+        Matrix m = new(a.Rows, b.Columns);
+
+        for (int i = 0; i < a.Rows; i++)
+        {
+            for (int j = 0; j < b.Columns; j++)
+            {
+                double[] row = a.GetRow(i).ToArray();
+                m[i, j] = b.GetCol(j).Select((e, i) => e * row[i]).Sum();
+            }
+        }
+        return m;
+    }
+
+    public override string ToString()
+    {
+        System.Text.StringBuilder sb = new();
+        for (int i = 0; i < Rows; i++)
+        {
+            sb.Append('[');
+            for (int j = 0; j < Columns; j++)
+            {
+                sb.Append(values[i, j]);
+                if (j != Columns - 1)
+                    sb.Append(' ');
+            }
+            sb.AppendLine("]");
+        }
+
+        return sb.ToString().TrimEnd('\n').TrimEnd('\r');
+    }
+}
--- NEAConsole/Program.cs
+++ NEAConsole/Program.cs
@@ -1,8 +1,242 @@
-﻿namespace NEAConsole;
+﻿using System;
+using NEAConsole.Matrices;
+
+namespace NEAConsole;
 internal class Program
 {
+    static void ListChoices(string[] options, int chosen)
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            Console.WriteLine($"{(i == chosen ? ">" : " ")} {options[i]} {(i == chosen ? "<" : " ")}");
+        }
+    }
+
+    static int Choose(string[] options)
+    {
+        int choice = 0;
+        while (true)
+        {
+            Console.Clear();
+            ListChoices(options, choice);
+            ConsoleKey key = Console.ReadKey().Key;
+            switch (key)
+            {
+                case ConsoleKey.UpArrow: choice = choice == 0 ? choice : choice - 1; break;
+                case ConsoleKey.DownArrow: choice = choice == options.Length - 1 ? choice : choice + 1; break;
+                case ConsoleKey.Enter: return choice;
+            }
+        }
+    }
+
+    static void DrawMatrix(Matrix m) // naive; needs centering cols based on number widths
+    {
+        int xIndent = Console.CursorLeft;
+        int initY = Console.CursorTop;
+        for (int i = 0; i < m.Rows; i++)
+        {
+            Console.Write('[');
+            for (int j = 0; j < m.Columns; j++)
+            {
+                Console.Write($"{m[i, j]}{(j == m.Columns - 1 ? ']' : " ")}");
+            }
+            if (i < m.Rows - 1)
+            {
+                Console.CursorLeft = xIndent;
+                Console.CursorTop++;
+            }
+        }
+
+        Console.CursorTop = initY;
+    }
+
+
+
+    static void DrawInputMatrix(string[][] m, int x, int y)
+    {
+        int xIndent = Console.CursorLeft;
+        int yIndent = Console.CursorTop;
+
+        for (int i = 0; i < m.Length; i++)
+        {
+            Console.Write('[');
+            for (int j = 0; j < m[0].Length; j++)
+            {
+                Console.BackgroundColor = (i == y && j == x) ? ConsoleColor.Gray : ConsoleColor.DarkGray;
+                Console.Write(m[i][j]);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write(' ');
+            }
+            Console.CursorLeft--;
+            Console.Write(']');
+            Console.CursorLeft = xIndent;
+            Console.CursorTop++;
+        }
+
+        Console.CursorTop = yIndent;
+    }
+
+    static string[][] EmptyInputMat(int rows, int cols) // the pains of a 2d array
+    {
+        string[][] inputs = new string[rows][];
+
+        for (int i = 0; i < rows; i++)
+        {
+            inputs[i] = new string[cols];
+            for (int j = 0; j < cols; j++)
+            {
+                inputs[i][j] = " ";
+            }
+        }
+
+        return inputs;
+    }
+
+    static Matrix InputMatrix(int rows, int cols)
+    {
+        Console.CursorVisible = false;
+
+        Matrix? m = null!; // compilation purposes, wont ever return null
+        string[][] inputs = EmptyInputMat(rows, cols); // why is it a jagged array you ask? so i can select through it
+
+        int x = 0, y = 0;
+        DrawInputMatrix(inputs, x, y);
+
+        bool entering = true;
+        bool redraw = false;
+        while (entering)
+        {
+            var key = Console.ReadKey(true);
+            switch (key.Key)
+            {
+                case ConsoleKey.RightArrow:
+                    if (x < cols - 1) x++;
+                    redraw = true;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (x > 0) x--;
+                    redraw = true;
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (y < rows - 1) y++;
+                    redraw = true;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (y > 0) y--;
+                    redraw = true;
+                    break;
+
+
+                case ConsoleKey.Enter:
+                case ConsoleKey.Spacebar:
+                    if (x < cols - 1) x++;
+                    else (x, y) = (0, y + 1);
+                    redraw = true;
+                    break;
+
+                case ConsoleKey.Escape:
+                    entering = false;
+                    break;
+
+
+
+                case ConsoleKey.Backspace:
+                    if (inputs[x][y].Length > 0)
+                    {
+                        
+                    }
+                    break;
+
+            }
+            if (redraw) DrawInputMatrix(inputs, x, y);
+
+            if (!entering)
+            {
+                try
+                {
+                    var doubles = inputs.Select(r => r.Select(e => double.Parse(e)));
+                    m = new Matrix(rows, cols, doubles);
+                }
+                catch (FormatException)
+                {
+                    entering = true;
+                }
+            }
+        }
+
+        Console.CursorVisible = true;
+        return m;
+    }
+
+    static void MatricesTest()
+    {
+        Console.Clear();
+
+        (int rows, int cols) = (Random.Shared.Next(1, 4), Random.Shared.Next(1, 4));
+        Matrix mat1 = new(rows, cols, Enumerable.Range(0, rows * cols).Select(n => (double)Random.Shared.Next(-10, 10)));
+        Matrix mat2 = new(rows, cols, Enumerable.Range(0, rows * cols).Select(n => (double)Random.Shared.Next(-10, 10)));
+
+        DrawMatrix(mat1);
+        
+        Console.CursorTop++;
+        Console.Write(" + ");
+        Console.CursorTop--;
+        DrawMatrix(mat2);
+
+        Console.CursorTop++;
+        Console.Write(" + ");
+        Console.CursorTop--;
+
+        Matrix answer = InputMatrix(rows, cols);
+    }
+
+    static void SimplexTest()
+    {
+
+    }
+
+    static void MathsMenu()
+    {
+
+    }
+    static void FMathsMenu()
+    {
+        string[] options = { "Matrices", "Simplex", "Hypothesis Testing", "Dijkstra's", "Prim's", "Return" };
+
+        Console.WriteLine("Choose a topic to be tested on");
+
+        while (true)
+        {
+            switch (Choose(options))
+            {
+                case 0: MatricesTest(); break;
+                case 1: SimplexTest(); break;
+                case 6: return;
+            }
+        }
+    }
+    static void CSciMenu()
+    {
+
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] options = { "Maths", "Further Maths", "Computer Science", "Exit" };
+
+        Console.WriteLine("Choose a subject to revise");
+
+        bool @continue = true;
+        while (@continue)
+        {
+            switch (Choose(options))
+            {
+                case 0: MathsMenu(); break;
+                case 1: FMathsMenu(); break;
+                case 2: CSciMenu(); break;
+                case 3: @continue = false; break;
+            }
+        }
+
     }
 }
